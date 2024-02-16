58d208c3c270def0de29a6cf929cfe59f102716c
unknown <ugwuchiagoziestanley@gmail.com>
2024-02-16 21:41:49 +0000

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <React.Fragment>
+      <div className="ml">
+        <img style={{width: "27px", marginRight: ".3rem"}} src="/knit-logo.svg" className="w-5" />
+      </div>
+      <span className="text-gray-600 font-normal hidden md:inline"></span>
+    </React.Fragment>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/gbxnga/docs.useknit.io",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/gbxnga/docs.useknit.io",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Knit",
   },
-}
+};
 
-export default config
+export default config;
