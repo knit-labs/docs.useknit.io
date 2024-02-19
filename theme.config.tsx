ce5ec91deb0f0552da1e89abcdc4abd46d45c62d
unknown <ugwuchiagoziestanley@gmail.com>
2024-02-19 18:45:09 +0000

    updated page header

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,23 +1,54 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
-const config: DocsThemeConfig = {
+import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
+
+export default {
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      "https://docs.useknit.io" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <link rel="icon" href="/knit-logo.svg" />
+        <meta property="og:title" content={frontMatter.title || "Knit"} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || "The next site builder"}
+        />
+      </>
+    );
+  },
   logo: (
     <React.Fragment>
       <div className="ml">
-        <img style={{width: "27px", marginRight: ".3rem"}} src="/knit-logo.svg" className="w-5" />
+        <img
+          style={{ width: "27px", marginRight: ".3rem" }}
+          src="/knit-logo.svg"
+          className="w-5"
+        />
       </div>
       <span className="text-gray-600 font-normal hidden md:inline"></span>
     </React.Fragment>
   ),
-  // project: {
-  //   link: "https://github.com/gbxnga/docs.useknit.io",
-  // },
 
   docsRepositoryBase: "https://github.com/gbxnga/docs.useknit.io",
   footer: {
     text: "Knit",
   },
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – KNIT'
+      }
+    }
+  }
 };
 
-export default config;
+// export default config;
