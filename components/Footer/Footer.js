435289b4399d5392115366a4271ec95ed66ab94f
unknown <ugwuchiagoziestanley@gmail.com>
2024-08-29 14:19:20 +0100

    updated london address

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,17 +1,16 @@
 import React, { useEffect } from "react";
 import styles from "./Footer.module.scss";
 
-
 const Footer = () => {
-
   return (
     <div className={styles.footer}>
-  
       <div className={styles.footer__links}>
         <div className={styles.footer__links__group}>
           <h3>Products</h3>
           <p className={styles.footer__link}>
-            <a href="https://www.useknit.io/#payment-gateway">Payment Gateway</a>
+            <a href="https://www.useknit.io/#payment-gateway">
+              Payment Gateway
+            </a>
           </p>
           <p className={styles.footer__link}>
             <a href="https://www.useknit.io/#crypto-pos">Crypto P.O.S</a>
@@ -23,7 +22,9 @@ const Footer = () => {
             <a href="https://www.useknit.io/#direct-debit">Direct Debit</a>
           </p>
           <p className={styles.footer__link}>
-            <a href="https://www.useknit.io/#">Wallet as a Service (Request Access)</a>
+            <a href="https://www.useknit.io/#">
+              Wallet as a Service (Request Access)
+            </a>
           </p>
           <p className={styles.footer__link}>
             Banking as a Platform (Coming Soon)
@@ -77,12 +78,34 @@ const Footer = () => {
 
         <div className={styles.footer__links__group}>
           <h3>Contact</h3>
+          <p>info@useknit.io</p>
 
-          <p>
-            Knit Retail Technologies Ltd <br />
-            info@useknit.io <br />
-            Office One Coldbath Square, Farringdon <br /> London, United
-            Kingdom, EC1R 5HL
+          <p style={{ marginTop: ".7rem" }}>
+            <b
+              style={{
+                fontWeight: "700",
+                fontSize: ".9rem",
+              }}
+            >
+              Knit Business Technologies Ltd.
+            </b>
+            <br />
+            27 Old Gloucester Street, <br />
+            London, United Kingdom, WC1N 3AX
+          </p>
+          <p style={{}}>
+            <b
+              style={{
+                fontWeight: "700",
+                fontSize: ".9rem",
+                marginTop: ".5rem",
+              }}
+            >
+              Knit Business Financial Services Ltd.
+            </b>{" "}
+            <br />
+            3080 Yonge St <br />
+            Toronto ON, M4N 3N1, Canada
           </p>
         </div>
       </div>
